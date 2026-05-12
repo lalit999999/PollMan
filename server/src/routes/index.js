8a00fa9906f43f04f8da3ed4f3ff01aa8014b285
Lalit gurjar <gujarlalit79@gmail.com>
2026-05-13 01:33:27 +0530

    feat: add user routes to the main router for user-related endpoints

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import healthRoutes from "./health.routes.js";
 import authRoutes from "./auth.routes.js";
+import userRoutes from "./user.routes.js";
 
 const router = Router();
 
 router.use("/auth", authRoutes);
+router.use("/user", userRoutes);
 router.use("/", healthRoutes);
 
 export default router;
